ad8886099e36fb5092860d9d9b40f9813603d9ab
kamazra4ka <admin@kamazra4ka.nl>
2025-05-24 20:41:15 +0200

    feat(overlayImage): move out image validation and timestamp generation into utils.js

--- generation/visual/overlayImage.js
+++ generation/visual/overlayImage.js
@@ -1,37 +1,30 @@
 import Canvas from '@napi-rs/canvas'
 import {AttachmentBuilder} from "discord.js";
+import {getTimestamp, validateCanvasImage} from "../../utils.js";
 
-export const overlayImage = async (image1, image2, variant) => {
+export const overlayImage = async (image1, image2, variant, height) => {
     try {
 
-        if (image1.url) {
-            image1 = await Canvas.loadImage(image1.url)
-        } else if (typeof image1 === 'string') {
-            image1 = await Canvas.loadImage(image1)
-        }
+        image1 = await validateCanvasImage(image1, Canvas);
+        image2 = await validateCanvasImage(image2, Canvas);
 
-        if (image2.url) {
-            image2 = await Canvas.loadImage(image2.url)
-        } else if (typeof image2 === 'string') {
-            image2 = await Canvas.loadImage(image2)
-        }
-
-        const canvas = new Canvas.Canvas(image1.width, image1.height)
+        const canvas = new Canvas.Canvas(image1.width, image1.height + height || 0)
         const ctx = canvas.getContext('2d')
 
-        const timestamp = new Date().toISOString().replace(/[-:T]/g, '').slice(0, 15)
+        ctx.fillStyle = '#ffffff'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-        ctx.drawImage(image1, 0, 0)
+        ctx.drawImage(image1, 0, canvas.height - image1.height)
 
         switch (variant) {
             default:
-            case 'default':
-                ctx.drawImage(image2, 0, 0, image1.width, image2.height);
+            case 'speechbubble':
+                ctx.drawImage(image2, 0, 0 + height || 0, image1.width, image2.height);
                 break
         }
 
         const buffer = await canvas.encode('png');
-        return new AttachmentBuilder(buffer, {name: `meme_${timestamp}.png`});
+        return new AttachmentBuilder(buffer, {name: `meme_${getTimestamp()}.png`});
 
     } catch (error) {
         console.error('Error overlaying images:', error)

3453327093bc8eeca03fac1f243a0533a425b85e
kamazra4ka <admin@kamazra4ka.nl>
2025-05-29 15:39:59 +0200

    feat(greentext): finish generateGreentext.js logic

--- commands/iamlucky.js
+++ commands/iamlucky.js
@@ -1,5 +1,6 @@
 import {getRandomAvatar} from "../discord/getRandomAvatar.js";
 import {generateQuote} from "../generation/visual/generateQuote.js";
+import {generateGreentext} from "../generation/text/markov/generateGreentext.js";
 
 export const iamlucky = async (interaction) => {
     const image = await getRandomAvatar(interaction.guildId)
@@ -7,5 +8,6 @@ export const iamlucky = async (interaction) => {
     const result = await generateQuote(image, interaction.channelId, interaction.guildId);
     //  const result = await generateSpeechbubble(image, interaction.channelId);
 
-    await interaction.reply({files: [result]});
+    //await interaction.reply({files: [result]});
+    await interaction.reply({content: await generateGreentext(interaction.channelId)});
 };

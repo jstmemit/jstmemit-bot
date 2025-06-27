6276672d27e6e9e4d753539a363608b08a57b48c
kamazra4ka <admin@kamazra4ka.nl>
2025-06-27 18:28:56 +0200

    feat(handleNewMessages): remove 20% chance to use avatar (make it 100%)

--- discord/handlers/handleNewMessage.js
+++ discord/handlers/handleNewMessage.js
@@ -28,11 +28,7 @@ export const handleNewMessage = async (interaction) => {
         }
     } else {
         insertMessage(interaction.channelId, interaction.content);
-
-        // to sometimes insert avatars into memes, because without intents I cannot query avatars normally ;(
-        if (Math.random() < 0.2) {
-            insertMessage(interaction.channelId, interaction.member.displayAvatarURL({dynamic: true, size: 1024}));
-        }
+        insertMessage(interaction.channelId, interaction.member.displayAvatarURL({dynamic: true, size: 1024}));
     }
 
     await handleUnpromtedMeme(interaction, channelSettings);

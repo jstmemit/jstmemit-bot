64d4521463aa00a4ebfa0e97d3a3e325251c7441
kamazra4ka <admin@kamazra4ka.nl>
2025-06-30 20:01:33 +0200

    fix(handleNewEntitlement): fix bug with entitlement analytics 💀💀

--- src/discord/handlers/handleNewEntitlement.js
+++ src/discord/handlers/handleNewEntitlement.js
@@ -2,7 +2,6 @@ import {analytics as posthog} from "../../../bot.js";
 
 export const handleNewEntitlement = async (entitlement) => {
     await posthog.capture({
-        distinctId: interaction.channelId,
         event: 'entitlement_created',
         properties: {
             entitlementId: entitlement.id,

7fb8bec738351f073541a2bfa9b3e7e4a6c6f1b6
kamazra4ka <admin@kamazra4ka.nl>
2025-08-16 17:55:46 +0200

    refactor(ready): move bot activity into settings + add info message about it

--- src/discord/events/handlers/ready.js
+++ src/discord/events/handlers/ready.js
@@ -4,17 +4,19 @@ import {commands} from '#src/discord/commands/commands.js';
 import {analytics} from "#src/analytics/initializeAnalytics.js";
 import {getConfig} from "#src/generation/getConfig.js";
 import {log} from "../../../../bot.js";
+import {settings} from "#config/settings.js";
 
 
 export default {
     name: Events.ClientReady,
     once: true,
     async execute(client) {
-        log.info(`Logged in as ${client.user.tag}!`);
+        log.info(`Logged in as ${client.user.tag}! ID: ${client.user.id}`);
 
-        client.user.setActivity('how to make memes', {
+        client.user.setActivity(settings.activity, {
             type: ActivityType.Watching
         });
+        log.info(`Set activity to: ${settings.activity}`);
 
         // thanks posthog for taking 2 minutes to load analytics data
         getConfig()
@@ -22,7 +24,7 @@ export default {
         try {
             await initializeCommands(commands);
 
-            log.info('All commands declared successfully.');
+            log.info('All commands declared successfully');
             log.info('Bot is ready to use!');
         } catch (error) {
             log.error('Error declaring commands:', error);
